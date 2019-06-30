7381968d299e30494d61545886ab8c60edbabac7
Swati Ahuja <swatiahuja.ahuja@gmail.com>
2019-06-30 21:56:03 +0530

    Updates

---
+++ precache-manifest.b5690808a07bce7ddfc591a6e2bffd2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13049ca650659f4115a1f42c19286b22",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae6eb7e7b6c20504cd36",
+    "url": "/portfolio/static/css/main.144a64ec.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ae6eb7e7b6c20504cd36",
+    "url": "/portfolio/static/js/main.2ad65369.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
